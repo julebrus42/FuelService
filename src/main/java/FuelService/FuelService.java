2e7d29ec2187373d51ff41f3b3489d0ace803142
Petter <Pettersen_97@hotmail.com>
2020-10-20 14:28:35 +0200

    Get fuel station and delete

--- src/main/java/FuelService/FuelService.java
+++ src/main/java/FuelService/FuelService.java
@@ -5,13 +5,23 @@
  */
 package FuelService;
 
+import Objects.FuelStation;
+import java.util.List;
+import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.EntityManager;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 
 /**
  *
@@ -25,7 +35,26 @@ public class FuelService {
     @PersistenceContext
     EntityManager em;
     
-   
     
+   @GET
+    @Path("stations")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<FuelStation> getFuelStations() {
+        return em.createNamedQuery(FuelStation.FIND_ALL_FUELSTATIONS, FuelStation.class).getResultList();
+    }
     
-}
+    @DELETE
+    @Path("delete")
+    public Response delete(@QueryParam("stationId") Long stationId) {
+        FuelStation fuelStation = em.find(FuelStation.class, stationId);
+        if (fuelStation != null){
+                em.remove(fuelStation);
+                return Response.ok().build();
+            }
+           return Response.notModified().build();
+        }
+        
+     
+    }
+ 
+    
