5b1e4e2f402711c6b22aeac0b526e15e898dd251
Daniel Iversen <daiverse@stud.ntnu.no>
2020-10-20 14:45:58 +0200

    Updated addfuelstation

--- src/main/java/FuelService/FuelService.java
+++ src/main/java/FuelService/FuelService.java
@@ -9,10 +9,13 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.EntityManager;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
+import Objects.FuelStation;
+import javax.ws.rs.GET;
 /**
  *
  * @author danie
@@ -25,7 +28,38 @@ public class FuelService {
     @PersistenceContext
     EntityManager em;
     
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping")
+                .build();
+    }
+    
    
+    @POST
+    @Path("addFuelStation")
+    public Response addFuelStation(
+            @FormParam("name") String name,
+            @FormParam("coordinates") String coordinates,
+            @FormParam("petrolPrice") int petrolPrice,
+            @FormParam("dieselPrice") int dieselprice,
+            @FormParam("petrol") boolean petrol,
+            @FormParam("diesel") boolean diesel) {
+        
+        FuelStation fuelStation = new FuelStation();
+        
+        fuelStation.setName(name);
+        fuelStation.setCoordinates(coordinates);
+        fuelStation.setPetrolPrice(petrolPrice);
+        fuelStation.setDieselPrice(dieselprice);
+        fuelStation.setPetrol(petrol);
+        fuelStation.setDiesel(diesel);
+        
+        em.persist(fuelStation);
+        
+        return Response.ok().build();
+    }
     
     
 }
